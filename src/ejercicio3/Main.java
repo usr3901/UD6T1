5290bcb3ccf870edf6ea2261305eddb00540f72e
usr3901 <usr3901@salesianos-lacuesta.net>
2026-06-05 10:05:36 +0100

    Hecho el ejercicio 3

--- src/ejercicio3/Main.java
+++ src/ejercicio3/Main.java
@@ -1,5 +1,79 @@
 package ejercicio3;
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Main {
-    
+
+    public static void main(String[] args) {
+
+        ArrayList<Pelicula> peliculas = new ArrayList<>();
+
+        try {
+
+            BufferedReader br =
+                    new BufferedReader(
+                            new FileReader("peliculas.csv"));
+
+            br.readLine();
+
+            String linea;
+
+            while((linea = br.readLine()) != null){
+
+                String[] datos = linea.split(",");
+
+                peliculas.add(
+                        new Pelicula(
+                                datos[0],
+                                datos[1],
+                                Integer.parseInt(datos[2])
+                        )
+                );
+            }
+
+            br.close();
+
+            int totalPeliculas = peliculas.size();
+            int duracionTotal = 0;
+
+            Pelicula mayor = peliculas.get(0);
+
+            for(Pelicula p : peliculas){
+
+                duracionTotal += p.getDuracionMinutos();
+
+                if(p.getDuracionMinutos() > mayor.getDuracionMinutos()){
+                    mayor = p;
+                }
+            }
+
+            double media =
+                    (double) duracionTotal / totalPeliculas;
+
+            PrintWriter resumen =
+                    new PrintWriter("resumen_peliculas.txt");
+
+            resumen.println("TOTAL: " + totalPeliculas);
+            resumen.println("DURACION TOTAL: " + duracionTotal);
+            resumen.println("MEDIA: " + media);
+            resumen.println("MAS LARGA: " + mayor);
+
+            resumen.close();
+
+            FileWriter log =
+                    new FileWriter(
+                            "log_peliculas.txt",
+                            true);
+
+            log.write("Programa ejecutado correctamente\n");
+
+            log.close();
+
+            System.out.println("Archivos generados");
+
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
 }
--- src/ejercicio3/Pelicula.java
+++ src/ejercicio3/Pelicula.java
@@ -1,5 +1,31 @@
 package ejercicio3;
 
 public class Pelicula {
-    
+
+    private String titulo;
+    private String genero;
+    private int duracionMinutos;
+
+    public Pelicula(String titulo, String genero, int duracionMinutos) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracionMinutos = duracionMinutos;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    @Override
+    public String toString() {
+        return titulo + " - " + genero + " - " + duracionMinutos;
+    }
 }
